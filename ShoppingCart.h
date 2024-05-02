8d756c94086cc7bedfc813718ddbd84422cce51a
JohnVidal1 <167556389+JohnVidal1@users.noreply.github.com>
2024-05-02 14:45:23 -0500

    Update ShoppingCart.h

--- ShoppingCart.h
+++ ShoppingCart.h
@@ -1,36 +1,32 @@
 #ifndef SHOPPING_CART_H
 #define SHOPPING_CART_H
 
-#include <string>
 #include <vector>
-using namespace std;
-
+#include <string>
 #include "ItemToPurchase.h"
 
 class ShoppingCart {
-   public:
-      ShoppingCart();
-      ShoppingCart(string name, string date);
-      
-      string GetCustomerName() const;
-      string GetDate() const;
-      
-      void AddItem(ItemToPurchase item);
-      void RemoveItem(string name);
-      
-      void ModifyItem(ItemToPurchase item);
-      
-      int GetNumItemsInCart();
-      double GetCostOfCart();
-      
-      void PrintTotal();
-      void PrintDescriptions();
-      
-   
-   private:
-      string customerName;
-      string currentDate;
-      vector<ItemToPurchase> cartItems;
+public:
+    ShoppingCart();
+    ShoppingCart(std::string name, std::string date);
+
+    std::string GetCustomerName() const;
+    std::string GetDate() const;
+
+    void AddItem(const ItemToPurchase& item);
+    bool RemoveItem(const std::string& name);
+    bool ModifyItem(const ItemToPurchase& item);
+
+    int GetNumItemsInCart() const;
+    double GetCostOfCart() const;
+
+    void PrintTotal() const;
+    void PrintDescriptions() const;
+
+private:
+    std::string customerName;
+    std::string currentDate;
+    std::vector<ItemToPurchase> cartItems;
 };
 
 #endif
